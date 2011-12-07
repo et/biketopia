6689f203a31b7130a7dd9ad3d29969bb1f909981
Eric Thomas <eric.l.m.thomas@gmail.com>
2011-12-07 13:17:17 -0800

    initial refinery generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biketopia::Application.config.secret_token = '5de265fca4d93b8f9ee79e7146aadfe06dbd2fd33921a6fa3031546f3104aeeb1837b753b11c38470eccca5379a6f00bfa1465e729df855c02e09c4029a61e99'
